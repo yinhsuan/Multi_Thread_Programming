195e124b7948a8f68fbb959f276e59f60822313f
yinhsuan <sandy10235168@gmail.com>
2022-11-07 19:26:35 +0800

    SSE - final version

--- part1/pi.c
+++ part1/pi.c
@@ -3,10 +3,6 @@
 #include <time.h>
 #include <pthread.h>
 #include <immintrin.h>
-// #include "./sse2neon/sse2neon.h"
-// #include <xmmintrin.h>
-// #include <emmintrin.h>
-// #include <nmmintrin.h>
 // REFERENCE: https://github.com/f4exb/cm256cc/blob/master/sse2neon.h
 
 int threadCount = 0;
@@ -22,67 +18,26 @@ void* calculate (void* threadId) {
     int tid = (int)longTid;
     unsigned int seed = (long)threadId + 1;
 
-    // avx_xorshift128plus_key_t mykey;
-    // avx_xorshift128plus_init(324, 4444, &mykey);
-
-    // __m256 MAX = _mm256_set1_ps(INT32_MAX);
-    // __m256 ONES = _mm256_set1_ps(1.0f);
-    // __m128i MAX = _mm_set1_epi32(INT32_MAX);
-    // __m128i ONEI = _mm_set1_epi32(1);
-    // __m128 ONE = _mm_cvtepi32_ps(ONEI);
     __m128 ONE = _mm_set_ps1(1.f);
-
-    // __m256i randomX, randomY;
-    // __m256 floatX, floatY, randX, randY, distanceSquared, mask;
-    // unsigned int result[4];
-    __m128i randomX, randomY;
-    __m128 floatX, floatY, randX, randY, distanceSquared, mask;
+    __m128 randX, randY, distanceSquared, mask;
     unsigned int hits;
-    float val[4];
 
     for (long long toss=0; toss < tossNum; toss+=4) {
-        // Return a 256-bit random "number"
-        // randomX =  avx_xorshift128plus(&mykey);
-        // randomY =  avx_xorshift128plus(&mykey);
-        // rand_sse(result);
-        // _mm_storeu_si128((__m128i*) result, cur_seed);
-
-        // Converts 8 int to 8 float
-        // floatX = _mm256_cvtepi32_ps(randomX);
-        // floatY = _mm256_cvtepi32_ps(randomY);
-
-        // Get random number [-1, 1]
-        // randX = _mm256_div_ps(floatX, MAX);
-        // randY = _mm256_div_ps(floatY, MAX);
-
-
         randX = _mm_set_ps((float)rand_r(&seed)/RAND_MAX, (float)rand_r(&seed)/RAND_MAX, (float)rand_r(&seed)/RAND_MAX, (float)rand_r(&seed)/RAND_MAX);
         randY = _mm_set_ps((float)rand_r(&seed)/RAND_MAX, (float)rand_r(&seed)/RAND_MAX, (float)rand_r(&seed)/RAND_MAX, (float)rand_r(&seed)/RAND_MAX);
+
         distanceSquared = _mm_add_ps(_mm_mul_ps(randX, randX), _mm_mul_ps(randY, randY));
-        
-        // _mm_store_ps(val, distanceSquared);
-        // for (int i = 0; i < 4; i++){
-        //     if (val[i] <= 1.f) {
-        //         *localCircle += 1;
-        //     }
-        // }
         mask = _mm_cmple_ps(distanceSquared, ONE);
         hits = _mm_movemask_ps(mask);
         *localCircle += _mm_popcnt_u32(hits);
     }
-    // if numberOfTosses is not well devided by thread#
+
     if (tid == 0) {
         for (long long toss=0; toss<remainNum; toss+=4) {
             randX = _mm_set_ps((float)rand_r(&seed)/RAND_MAX, (float)rand_r(&seed)/RAND_MAX, (float)rand_r(&seed)/RAND_MAX, (float)rand_r(&seed)/RAND_MAX);
             randY = _mm_set_ps((float)rand_r(&seed)/RAND_MAX, (float)rand_r(&seed)/RAND_MAX, (float)rand_r(&seed)/RAND_MAX, (float)rand_r(&seed)/RAND_MAX);
             distanceSquared = _mm_add_ps(_mm_mul_ps(randX, randX), _mm_mul_ps(randY, randY));
 
-            // _mm_store_ps(val, distanceSquared);
-            // for (int i = 0; i < 4; i++){
-            //     if (val[i] <= 1.f) {
-            //         *localCircle += 1;
-            //     }
-            // }
             mask = _mm_cmple_ps(distanceSquared, ONE);
             hits = _mm_movemask_ps(mask);
             *localCircle += _mm_popcnt_u32(hits);
